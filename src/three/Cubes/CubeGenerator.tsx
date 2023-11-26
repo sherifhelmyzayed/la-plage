29f47243891cacdefc5cfd8d26850d93fc3d9200
sherifci <131358395+sherifci@users.noreply.github.com>
2023-11-27 03:08:22 +0400

    feat: infinity cubes

--- src/three/Cubes/CubeGenerator.tsx
+++ src/three/Cubes/CubeGenerator.tsx
@@ -1,17 +1,22 @@
 import { Vector3 } from 'three'
 import CubesCluster from './CubesCluster'
 import { useFrame } from '@react-three/fiber'
+import { useState } from 'react'
 
 const CubeGenerator = () => {
+    const [multiplier, setMultiplier] = useState(0)
 
     useFrame(({ camera }) => {
-        console.log(camera.position.z)
+        const x = Math.floor(camera.position.z / 32)
+        if (x !== multiplier) {
+            setMultiplier(x)
+        }
     })
     return (
         <>
-            <CubesCluster position={new Vector3(0, 0, -32)} />
-            <CubesCluster position={new Vector3(0, 0, 0)} />
-            <CubesCluster position={new Vector3(0, 0, 32)} />
+            <CubesCluster position={new Vector3(0, 0, multiplier * 32 - 32)} />
+            <CubesCluster position={new Vector3(0, 0, multiplier * 32)} />
+            <CubesCluster position={new Vector3(0, 0, multiplier * 32 + 32)} />
         </>
     )
 }
