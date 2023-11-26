5480726b73b9e8e696f2d189bcb7449106b1a89a
sherifci <131358395+sherifci@users.noreply.github.com>
2023-11-26 23:56:01 +0400

    feat: project initialize ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()
+  ],
+
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src') },
+      { find: '@context', replacement: path.resolve(__dirname, './src/context') },
+      { find: '@svg', replacement: path.resolve(__dirname, './src/svg') },
+      { find: '@three', replacement: path.resolve(__dirname, './src/three') },
+      { find: '@hooks', replacement: path.resolve(__dirname, './src/hooks') },
+      { find: '@functions', replacement: path.resolve(__dirname, './src/functions') },
+      { find: '@stores', replacement: path.resolve(__dirname, './src/stores') },
+      { find: '@assets', replacement: path.resolve(__dirname, './public/assets') },
+    ]
+  }
+
 })
