d4eab9248238caa87dcdfb0185acd2078afc45d5
sherifci <131358395+sherifci@users.noreply.github.com>
2023-11-27 03:02:44 +0400

    feat: added color cubes

--- src/three/Cubes/CubesCluster.tsx
+++ src/three/Cubes/CubesCluster.tsx
@@ -4,10 +4,10 @@ import { Vector3 } from 'three'
 const CubesCluster = ({ position }: { position: Vector3 }) => {
     return (
         <group position={position}>
-            <SingleCube position={new Vector3(0, 0, 0)} />
-            <SingleCube position={new Vector3(0, 0, 8)} />
-            <SingleCube position={new Vector3(0, 0, 16)} />
-            <SingleCube position={new Vector3(0, 0, 24)} />
+            <SingleCube position={new Vector3(0, 0, 0)} color={"green"}/>
+            <SingleCube position={new Vector3(0, 0, 8)} color={"red"}/>
+            <SingleCube position={new Vector3(0, 0, 16)} color={"yellow"}/>
+            <SingleCube position={new Vector3(0, 0, 24)} color={"blue"}/>
         </group>
     )
 }
--- src/three/Cubes/SingleCube.tsx
+++ src/three/Cubes/SingleCube.tsx
@@ -2,7 +2,7 @@ import { RigidBody } from '@react-three/rapier'
 import { BackSide, Vector3 } from 'three'
 import { useGLTF } from "@react-three/drei";
 
-const SingleCube = ({ position }: { position: Vector3 }) => {
+const SingleCube = ({ position, color }: { position: Vector3, color: string }) => {
   // @ts-ignore
   const { nodes } = useGLTF("/assets/Cube.glb");
   return (
@@ -14,7 +14,7 @@ const SingleCube = ({ position }: { position: Vector3 }) => {
           geometry={nodes.Cube.geometry}
           position={[0, 1, 0]}
         >
-          <meshStandardMaterial attach={`material`} color={"white"} side={BackSide} />
+          <meshStandardMaterial attach={`material`} color={color} side={BackSide} />
         </mesh>
       </group>
     </RigidBody>
