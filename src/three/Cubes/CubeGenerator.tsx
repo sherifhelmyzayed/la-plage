93caa93254f7e4d30b83f10e2d8755cfbb793ec2
sherifci <131358395+sherifci@users.noreply.github.com>
2023-11-27 02:58:53 +0400

    feat: added cluster

--- src/three/Cubes/CubeGenerator.tsx
+++ src/three/Cubes/CubeGenerator.tsx
@@ -1,10 +1,17 @@
 import { Vector3 } from 'three'
 import CubesCluster from './CubesCluster'
+import { useFrame } from '@react-three/fiber'
 
 const CubeGenerator = () => {
+
+    useFrame(({ camera }) => {
+        console.log(camera.position.z)
+    })
     return (
         <>
+            <CubesCluster position={new Vector3(0, 0, -32)} />
             <CubesCluster position={new Vector3(0, 0, 0)} />
+            <CubesCluster position={new Vector3(0, 0, 32)} />
         </>
     )
 }
